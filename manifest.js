5ac04dcc496e89e574f49cf2d33e43e6de83c7ef
s1ckooo <148199550+s1ckooo@users.noreply.github.com>
2024-06-04 23:02:32 +0800

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+    "name": "MyPortfolio",
+    "short_name": "Portfolio",
+    "start_url": "/",
+    "display": "standalone",
+    "background_color": "#ffffff",
+    "theme_color": "#333333",
+    "icons": [
+      {
+        "src": "/images/icons/icon-192x192.png",
+        "type": "image/png",
+        "sizes": "192x192"
+      },
+      {
+        "src": "/images/icons/icon-512x512.png",
+        "type": "image/png",
+        "sizes": "512x512"
+      }
+    ]
+  }
+  
---
+++ sw.js
@@ -0,0 +1,27 @@
+const cacheName = 'myportfolio-cache-v1';
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/app.js',
+    '/images/icons/icon-192x192.png',
+    '/images/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(cache => {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
